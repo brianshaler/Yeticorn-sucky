c4a4a797bb6e528b0462caa28cfdb79a819f457e
Brian Shaler <brian@shaler.name>
2012-11-11 14:36:30 -0500

    cleanup

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -475,67 +475,7 @@ window.require.define({"models/game": function(exports, require, module) {
     }
 
     Game.prototype.defaults = {
-      tiles: [
-        {
-          positionX: 0,
-          positionY: 0,
-          player: {
-            name: "Hi",
-            color: "red"
-          }
-        }, {
-          positionX: 1,
-          positionY: 0
-        }, {
-          positionX: 2,
-          positionY: 0
-        }, {
-          positionX: 3,
-          positionY: 0
-        }, {
-          positionX: 0,
-          positionY: 1
-        }, {
-          positionX: 1,
-          positionY: 1
-        }, {
-          positionX: 2,
-          positionY: 1
-        }, {
-          positionX: 3,
-          positionY: 1,
-          hasCard: true
-        }, {
-          positionX: 0,
-          positionY: 2
-        }, {
-          positionX: 1,
-          positionY: 2
-        }, {
-          positionX: 2,
-          positionY: 2
-        }, {
-          positionX: 3,
-          positionY: 2,
-          player: {
-            name: "Yo",
-            color: "blue"
-          },
-          hasCard: true
-        }, {
-          positionX: 0,
-          positionY: 3
-        }, {
-          positionX: 1,
-          positionY: 3
-        }, {
-          positionX: 2,
-          positionY: 3
-        }, {
-          positionX: 3,
-          positionY: 3
-        }
-      ]
+      tiles: []
     };
 
     return Game;
